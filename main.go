bf31bc3e6041a5a7576d25b6cac37b586b9cb927
Davit Munjishvili <dmunjishvili@impel.ai>
2025-06-23 13:55:44 +0400

    fix tls thingling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"embed"
 	"encoding/json"
 	"fmt"
@@ -39,26 +40,36 @@ var centers = map[int]string{
 
 func fetchDatesForCenter(centerID int, centerName string, ch chan<- centerResult, wg *sync.WaitGroup) {
 	defer wg.Done()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
 	url := fmt.Sprintf("https://api-my.sa.gov.ge/api/v1/DrivingLicensePracticalExams2/DrivingLicenseExamsDates2?CategoryCode=4&CenterId=%d", centerID)
 	result := centerResult{CenterName: centerName}
-	resp, err := http.Get(url)
+
+	resp, err := client.Get(url)
 	if err != nil {
 		result.Error = fmt.Sprintf("Error fetching data: %v", err)
 		ch <- result
 		return
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		result.Error = fmt.Sprintf("API returned non-200 status: %s", resp.Status)
 		ch <- result
 		return
 	}
+
 	var datesResponse []availableDate
 	if err := json.NewDecoder(resp.Body).Decode(&datesResponse); err != nil {
 		result.Error = fmt.Sprintf("Error decoding JSON: %v", err)
 		ch <- result
 		return
 	}
+
 	result.Dates = datesResponse
 	ch <- result
 }
